d90a334df43f5e20c20a7ba81ebfc9a968e4f088
Matteo Bilotta <me@byloth.net>
2023-01-03 16:12:07 +0100

    [FIX] Minor fixes to the library. (RC.2)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { Exception, HandledException } from "./exceptions";
 export { HandlerBuilder } from "./models";
 
+export const VERSION = "2.0.0-rc.2";
+
 export type { HandlerOptions } from "./models";
 export type { ExceptionHandler } from "./types";
