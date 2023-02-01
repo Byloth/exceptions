53fea6b02d3b1057aa2107e0a8e82207719b2325
Matteo Bilotta <me@byloth.net>
2023-02-01 17:07:08 +0100

    [UPD] Updated dependencies. + Published new `next` release. 🔝

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 export { Exception } from "./exceptions/core.js";
 export { HandledException } from "./exceptions/index.js";
-export { NetworkException, RuntimeException, ValueException } from "./exceptions/types.js";
+export { NetworkException, ReferenceException, RuntimeException, ValueException } from "./exceptions/types.js";
 
 export { HandlerBuilder } from "./models.js";
 
 export { handle } from "./helpers.js";
 
-export const VERSION = "2.0.0-rc.5";
+export const VERSION = "2.0.0-rc.7";
 
 export type { HandlerOptions } from "./models.js";
 export type { ErrorHandler } from "./types.js";
